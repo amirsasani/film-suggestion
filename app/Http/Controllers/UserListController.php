1ad7f9277f5dafa262065ccd00f03af23cad9e9e
Amir Sasani <amir.ramin76@gmail.com>
2020-11-13 23:45:41 +0330

    implement titles suggestion in user list

--- app/Http/Controllers/UserListController.php
+++ app/Http/Controllers/UserListController.php
@@ -31,9 +31,49 @@ class UserListController extends Controller
 
     public function show(UserList $list) {
         $list = $list->load('titles');
-        $titles_to_add = Title::whereNotIn('id', $list->titles->pluck('id')->toArray())->inRandomOrder()->limit(5)->get();
+
+        $titles_to_add = $this->suggestUserTitlesToAdd($list);
 
         return view('user-lists.show', compact('list', 'titles_to_add'));
+    }
+
+    private function suggestUserTitlesToAdd(UserList $list) {
+        $titles_to_add = collect();
+
+        $this_list_titles = $list->titles;
+        $all_lists = UserList::with('titles')->whereNotIn('id', auth()->user()->lists()->inRandomOrder()->limit(40)->get()->pluck('id'))->get();
+
+        $_sorted_lists = [];
+        $collect_suggestions = false;
+        foreach ($all_lists as $_list) {
+            $intersect = $_list->titles->intersect($this_list_titles)->pluck('id')->toArray();
+            $difference = $_list->titles->whereNotIn('imdb_id', $_list->titles->intersect($this_list_titles)->pluck('imdb_id'))->pluck('id')->toArray();
+
+            if(!$collect_suggestions) {
+                $collect_suggestions = count($difference) > 0;
+            }
+
+            $_sorted_lists[] = compact('intersect', 'difference');
+        }
+
+        if ($collect_suggestions) {
+            usort($_sorted_lists, function ($a, $b) {
+                return (count($b['intersect']) - count($a['intersect']));
+            });
+
+            foreach ($_sorted_lists as $sorted_list){
+                $titles_to_add = $titles_to_add->merge($sorted_list['difference']);
+            }
+
+            $titles_to_add = $titles_to_add->unique();
+
+        } else {
+             $titles_to_add = Title::whereNotIn('id', $list->titles->pluck('id')->toArray())->inRandomOrder()->limit(5)->pluck('id')->toArray();
+        }
+
+        $titles_to_add = Title::find($titles_to_add);
+
+        return $titles_to_add;
     }
 
     public function addTitleToList(Request $request, UserList $list, Title $title) {
--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -23,19 +23,17 @@
                     </div>
                 </li>
 
-                @auth
-                    <li class="nav-item dropdown">
-                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
-                           data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                            Your Lists
-                        </a>
+                <li class="nav-item dropdown">
+                    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                       data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                        Your Lists
+                    </a>
 
-                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item" href="{{ route('user-lists.index') }}">All</a>
-                            <a class="dropdown-item" href="{{ route('user-lists.insert.form') }}">Add new</a>
-                        </div>
-                    </li>
-                @endauth
+                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="{{ route('user-lists.index') }}">All</a>
+                        <a class="dropdown-item" href="{{ route('user-lists.insert.form') }}">Add new</a>
+                    </div>
+                </li>
 
 
             </ul>
