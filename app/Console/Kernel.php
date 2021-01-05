43b640ad0ad3a631e387c98187b5100df840e00c
Amir Sasani <sasani.amir.1998@gmail.com>
2021-01-05 05:53:42 +0330

    set imdb:populate command schedule

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -19,12 +19,15 @@ class Kernel extends ConsoleKernel
     /**
      * Define the application's command schedule.
      *
-     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
+     * @param  Schedule  $schedule
      * @return void
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->command('imdb:populate')
+            ->timezone('Asia/Tehran')
+            ->tuesdays()
+            ->at('06:15');
     }
 
     /**
