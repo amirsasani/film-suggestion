39b253e2690efe878c41aa9556bc9a87bf89c415
Amir Sasani <sasani.amir.1998@gmail.com>
2021-01-05 06:05:41 +0330

    use https if environment is production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,7 +25,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //URL::forceScheme('https');
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
+        
         Paginator::useBootstrap();
     }
 }
