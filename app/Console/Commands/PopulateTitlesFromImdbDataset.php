0a87d922cc4aa49ea1986c3677ab4f81879072e2
Amir Sasani <sasani.amir.1998@gmail.com>
2021-01-05 18:28:02 +0330

    log added titles from imdb:populate

--- app/Console/Commands/PopulateTitlesFromImdbDataset.php
+++ app/Console/Commands/PopulateTitlesFromImdbDataset.php
@@ -5,6 +5,7 @@ namespace App\Console\Commands;
 use App\Services\Imdb\Handler;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
 
 class PopulateTitlesFromImdbDataset extends Command
 {
@@ -39,9 +40,10 @@ class PopulateTitlesFromImdbDataset extends Command
      *
      * @return int
      */
-    public function handle()
+    public function handle(): int
     {
         $this->alert('Downloading rating dataset');
+        Log::alert('Downloading rating dataset');
 
         $input = gzopen($this->dataset_url, 'r');
 
@@ -49,11 +51,14 @@ class PopulateTitlesFromImdbDataset extends Command
         while ($row = fgetcsv($input, 0, "\t")) {
             if ($i > 0) {
                 $row[] = $i;
-                dump($row);
                 $id = $row[0];
 
                 $imdb = new \Imdb\Title($id);
                 $title = Handler::insertTitle($imdb);
+
+                $log_msg = sprintf('title #%s added', $title->id);
+                $this->info($log_msg);
+                Log::info($log_msg);
             }
             $i++;
         }
