68d33d8020cb2c6ea1ff042e7c4050fa24bdfcf0
Amir Sasani <sasani.amir.1998@gmail.com>
2021-01-05 06:07:32 +0330

    yeah it works :)

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,7 +26,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         if ($this->app->environment('production')) {
-            dd('test');
             URL::forceScheme('https');
         }
 
