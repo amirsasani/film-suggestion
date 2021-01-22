dea111871facca0614b939c301ec42b5d3cf045c
Amir Sasani <sasani.amir.1998@gmail.com>
2021-01-23 00:03:36 +0330

    change ImdbImport chunkSize to 1000

--- app/Imports/TitlesImport.php
+++ app/Imports/TitlesImport.php
@@ -36,7 +36,7 @@ class TitlesImport implements ToModel, WithHeadingRow, WithChunkReading, ShouldQ
 
     public function chunkSize(): int
     {
-        return 4000;
+        return 1000;
     }
 
 }
